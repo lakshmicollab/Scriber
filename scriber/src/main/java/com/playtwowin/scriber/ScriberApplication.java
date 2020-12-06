d907ebfca7bbe0358d974f9333b97835a40726df
Deron Franklin Sobers II <deronfsobers2@gmail.com>
2020-12-06 16:21:20 -0600

    initial setup

---
+++ scriber/src/main/java/com/playtwowin/scriber/ScriberApplication.java
@@ -0,0 +1,13 @@
+package com.playtwowin.scriber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScriberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScriberApplication.class, args);
+	}
+
+}
