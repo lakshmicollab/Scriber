eb13214bce403c63a003ea6d3be1456a1f75eb14
Deron Franklin Sobers II <deronfsobers2@gmail.com>
2020-12-09 17:58:25 -0600

    added file upload api

--- scriber/src/main/java/com/playtwowin/scriber/controller/ScriberController.java
+++ scriber/src/main/java/com/playtwowin/scriber/controller/ScriberController.java
@@ -1,13 +1,25 @@
 package com.playtwowin.scriber.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
 
 @RestController
 public class ScriberController {
 
-    @GetMapping
+    @GetMapping("/scribe")
     public String scribeApi(){
         return "scriber";
+    }
+
+//    curl -k -X POST -F 'image=@/Pictures/running_cheetah.jpg' -v  http://localhost:8080/upload/
+    @PostMapping("/upload")
+    public String fileUploader(@RequestParam("file") MultipartFile multipartFile) {
+        String fileName = multipartFile.getOriginalFilename();
+        return fileName;
     }
 }
