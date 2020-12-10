dcab72ca4c16aedb61683e37554bbb91793d15ba
markrubio632 <mark.rubio.632@gmail.com>
2020-12-09 20:52:04 -0700

    added in another controller

--- scriber/src/main/java/com/playtwowin/scriber/controller/ScriberController.java
+++ scriber/src/main/java/com/playtwowin/scriber/controller/ScriberController.java
@@ -1,36 +1,19 @@
 package com.playtwowin.scriber.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-
-import com.playtwowin.model.User;
-import com.playtwowin.repo.UserRepository;
-
 @RestController
 public class ScriberController {
-	
-	@Autowired
-	UserRepository userRepo;
 
     @GetMapping("/scribe")
     public String scribeApi(){
         return "scriber";
     }
-    @PostMapping
-    public ResponseEntity<String> saveUser(@RequestParam String userName,@RequestParam String password,@RequestParam String firstName,@RequestParam String lastName,
-    		@RequestParam String email){
-    	userRepo.save(new User(0,userName, password, firstName, lastName, email));
-    	
-    	return new ResponseEntity<String>("New User added to Database", HttpStatus.OK);}
-
+	 
 //    curl -k -X POST -F 'image=@/Pictures/running_cheetah.jpg' -v  http://localhost:8080/upload/
     @PostMapping("/upload")
     public String fileUploader(@RequestParam("file") MultipartFile multipartFile) {
